c9e5934d627e9af645550fa33c9ff4cba3b58747
HeidenreichHailee <106415551+HeidenreichHailee@users.noreply.github.com>
2023-12-30 04:41:57 -0800

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'dff72fda-360d-46ed-9a59-139cc1c96957'}
 WEB_USERNAME=${WEB_USERNAME:-'admin'}
 WEB_PASSWORD=${WEB_PASSWORD:-'password'}
 
